4db795b53306518e10ae4bf142735dff72add29d
Maksiks <maksiksking@gmail.com>
2024-05-20 22:34:24 +0300

    h

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeButton = document.getElementsByClassName("darkMode")[0];
+
+  darkModeButton.addEventListener("click", () => {
+
+  });
+});
